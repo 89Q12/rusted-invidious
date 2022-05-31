43374bbbc25ba8d062d47361c0745de9d323f92e
11tuvork28 <zofi@xamh.de>
2022-05-31 22:02:29 +0200

    added bare bones logging filter helper method

---
+++ src/logging/filters.rs
@@ -0,0 +1,7 @@
+use tracing_subscriber::EnvFilter;
+/// Constructs the filter to log specific events/spans 
+/// https://docs.rs/tracing-subscriber/0.3.11/tracing_subscriber/struct.EnvFilter.html
+/// TODO: implement env variables instead of predefined target levels
+pub fn construct_logging_filter() -> EnvFilter{
+    EnvFilter::new( "warn,tokio::net=debug,tower_http=debug,db=info,server=trace")
+}
