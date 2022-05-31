317f817f6decb0d1c999e788f81f5615dd5deeeb
11tuvork28 <zofi@xamh.de>
2022-05-31 17:10:22 +0200

    added fundamental roues

---
+++ src/routes/routes.rs
@@ -0,0 +1,46 @@
+use axum::{
+    routing::{get, post},
+    Router,
+};
+pub fn channel_routes() -> Router{
+    Router::new()
+    .route("/:ucid", get(|| async {}))
+    .route("/:ucid/home", get(|| async {}))
+    .route("/:ucid/videos", get(|| async {}))
+    .route("/:ucid/playlists", get(|| async {}))
+    .route("/:ucid/community", get(|| async {}))
+    .route("/:ucid/about", get(|| async {}))
+    .route("/:ucid/live", get(|| async{}))
+}
+pub fn user_and_c_routes() -> Router{
+    Router::new()
+    .route("/:user/videos", get(|| async {}))
+    .route("/:user/playlists", get(|| async {}))
+    .route("/:user/community", get(|| async {}))
+    .route("/:user/about", get(|| async {}))
+}
+
+pub fn get_router() -> Router{
+    Router::new()
+    .route("/", get(|| async {}))
+    .nest("/channel", channel_routes())
+    .nest("/c", user_and_c_routes())
+
+    .nest("/user", user_and_c_routes())
+
+    .route("/attribution_link", get(|| async {}))
+
+    .route("/watch", get(|| async {}))
+    .route("/watch/:id", get(|| async {}))
+    .route("/watch/:id", get(|| async {}))
+    .route("/shorts/:id", get(|| async {}))
+    .route("/clip/:clip", get(|| async {}))
+    .route("/w/:id", get(|| async {}))
+    .route("/v/:id", get(|| async {}))
+    .route("/e/:id", get(|| async {}))
+
+    .route("search", get(|| async {}))
+    .route("results", get(|| async {}))
+
+
+}
