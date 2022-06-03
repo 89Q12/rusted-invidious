b64603d5ecc1577d8629270c93736f8e5ffa88d3
11Tuvork28 <zofi@xamh.de>
2022-06-03 19:59:21 +0200

    added comments

--- src/database/models/user.rs
+++ src/database/models/user.rs
@@ -4,8 +4,8 @@ use scylla::cql_to_rust::FromCqlVal;
 /// Represents a user queried from the database
 #[derive(Debug,IntoUserType, FromUserType)]
 pub struct User {
-    uuid: String, // Primary key
-    name: String,
+    uuid: String, // partition key
+    name: String, // clustering key
     password: String,
     token: String,
     feed_needs_update: bool,
