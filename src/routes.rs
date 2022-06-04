fc36ba2f4177b15eca59143403463e69b9547a08
11tuvork28 <zofi@xamh.de>
2022-06-04 12:23:32 +0200

    added handlers to route definitions

--- src/routes.rs
+++ src/routes.rs
@@ -3,47 +3,49 @@ use axum::{
     Router,
 };
 // Imports of the handlers
-use crate::handlers::home::{index};
+use crate::handlers::{home,feed,channel, video};
 
 /// Short hand for nested /channel routes
 fn channel_routes() -> Router{
     Router::new()
-    .route("/:ucid", get(|| async {}))
-    .route("/:ucid/home", get(|| async {}))
-    .route("/:ucid/videos", get(|| async {}))
-    .route("/:ucid/playlists", get(|| async {}))
-    .route("/:ucid/community", get(|| async {}))
-    .route("/:ucid/about", get(|| async {}))
-    .route("/:ucid/live", get(|| async{}))
+    .route("/:ucid", get(channel::index))
+    .route("/:ucid/home", get(channel::index))
+    .route("/:ucid/videos", get(channel::videos))
+    .route("/:ucid/playlists", get(channel::playlists))
+    .route("/:ucid/community", get(channel::community))
+    .route("/:ucid/about", get(channel::index))
+    .route("/:ucid/live", get(channel::live))
 }
 /// Short hand for nested /c and /user routes
 fn user_and_c_routes() -> Router{
     Router::new()
-    .route("/:user", get(|| async {}))
-    .route("/:user/videos", get(|| async {}))
-    .route("/:user/playlists", get(|| async {}))
-    .route("/:user/community", get(|| async {}))
-    .route("/:user/about", get(|| async {}))
+    .route("/:user",get(channel::index))
+    .route("/:user/home",get(channel::index))
+    .route("/:user/videos",get(channel::videos))
+    .route("/:user/playlists",get(channel::playlists))
+    .route("/:user/community",get(channel::community))
+    .route("/:user/about",get(channel::index))
+    .route("/:user/live",get(channel::live))
 }
 /// Create the main router struct used for all routes
 pub fn get_router() -> Router{
     Router::new()
-    .route("/", get(index))
+    .route("/", get(home::index))
     .nest("/channel", channel_routes())
 
     .nest("/c", user_and_c_routes())
     .nest("/user", user_and_c_routes())
 
-    .route("/attribution_link", get(|| async {}))
+    .route("/attribution_link", post(|| async {}))
     
-    .route("/watch", get(|| async {}))
-    .route("/watch_ajax", get(|| async {}))
-    .route("/watch/:id", get(watch))
-    .route("/shorts/:id", get(|| async {}))
-    .route("/clip/:clip", get(|| async {}))
-    .route("/w/:id", get(|| async {}))
-    .route("/v/:id", get(|| async {}))
-    .route("/e/:id", get(|| async {}))
+    .route("/watch", get(video::watch_v))
+    .route("/watch_ajax", get(video::watch_v))
+    .route("/watch/:id", get(video::redirect))
+    .route("/shorts/:id", get(video::redirect))
+    .route("/clip/:clip", get(video::clip))
+    .route("/w/:id", get(video::redirect))
+    .route("/v/:id", get(video::redirect))
+    .route("/e/:id", get(video::redirect))
     
     .route("/search", get(|| async {}))
     .route("/results", get(|| async {}))
