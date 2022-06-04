b043ef2260ee2829100f817e716846ca9e3deb04
11tuvork28 <zofi@xamh.de>
2022-06-04 12:23:45 +0200

    made functions public

--- src/handlers/video.rs
+++ src/handlers/video.rs
@@ -3,12 +3,12 @@ use axum::{response::{Response, Redirect}, Extension,extract::{path::Path, Query
 use crate::config::State;
 
 /// Handler for the /watch?v=id path and renders the watch page
-async fn watch_v(Extension(state): Extension<Arc<State>>,Query(params): Query<HashMap<String, String>>){
+pub async fn watch_v(Extension(state): Extension<Arc<State>>,Query(params): Query<HashMap<String, String>>){
     todo!()
 }
 
 /// Handler for the paths /w/:id, /v/:id,/e/:id, /shorts/:id and /watch/:id it redirects to the /watch/v=id path
-async fn redirect(Extension(state): Extension<Arc<State>>,Path(id): Path<String>) -> Redirect{
+pub async fn redirect(Extension(state): Extension<Arc<State>>,Path(id): Path<String>) -> Redirect{
     todo!()
 }
 
@@ -19,6 +19,6 @@ async fn watch_ajax(Extension(state): Extension<Arc<State>>,body: String){
 }
 
 /// Handler for the paths /clip/:id it extracts the videoId from the clip id and redirects to the /watch/v=id path
-async fn clip(Extension(state): Extension<Arc<State>>,Path(id): Path<String>) -> Redirect{
+pub async fn clip(Extension(state): Extension<Arc<State>>,Path(id): Path<String>) -> Redirect{
     todo!()
 }
