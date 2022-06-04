2372f41ef73cda5d227c1a5c2ea225c1afbfac0c
11tuvork28 <zofi@xamh.de>
2022-06-04 19:51:03 +0200

    added home handlers

--- src/handlers/home.rs
+++ src/handlers/home.rs
@@ -1,8 +1,20 @@
-use axum::response::Redirect;
+use std::sync::Arc;
+use axum::{response::Redirect, Extension,extract::path::Path, response::Response};
+use crate::config::State;
 /// Handler for the root path.
 /// Ideally this should be redirect the user to the configured home path
-/// e.g. /feed/popular
-pub async fn index()-> Redirect {
+/// e.g. /feed/popular or serve search page I guess but this could be changed
+pub async fn index(Extension(state): Extension<Arc<State>>)-> Response {
     // TODO: implement
-Redirect::temporary("/feed/popular")
+    todo!()
+}
+
+/// Handler for the /feed/popular path.
+pub async fn popular(Extension(state): Extension<Arc<State>>) -> Response {
+    todo!()
+}
+
+/// Handler for the /feed/trending path.
+pub async fn trending(Extension(state): Extension<Arc<State>>) -> Response {
+    todo!()
 }
