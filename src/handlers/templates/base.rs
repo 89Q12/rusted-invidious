f545cfc088d5ddfa5cd1c63ff0d0bca188ed1c1b
11tuvork28 <zofi@xamh.de>
2022-06-28 17:16:11 +0200

    made base public for easier inheritance

--- src/handlers/templates/base.rs
+++ src/handlers/templates/base.rs
@@ -4,12 +4,12 @@ use crate::{config::{Config, Preferences}, structs::user::User};
 #[derive(Template)] // this will generate the code...
 #[template(path = "base.html")]
 pub struct Base<'a>{
-    title: String,
-    config: &'a Config,
-    user: Option<User>,
-    preferences: Preferences,
-    search_bar: Option<String>,
-    current_page: String,
+    pub title: String,
+    pub config: &'a Config,
+    pub user: Option<User>,
+    pub preferences: Preferences,
+    pub search_bar: Option<String>,
+    pub current_page: String,
     #[locale]
     loc: Locale<'a>,
 }
