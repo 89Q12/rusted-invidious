371a2b8aec007ab3c82d726a74360235adf0e363
11tuvork28 <zofi@xamh.de>
2022-06-28 12:24:26 +0200

    added config

--- src/config.rs
+++ src/config.rs
@@ -1,24 +1,49 @@
 use crate::database::db_manger::DbManager;
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+use std::{collections::HashMap, fs};
 use youtubei_rs::types::{client::ClientConfig, query_results::BrowseResult};
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct Config {
     pub db_host: String,
     pub db_port: u16,
+    pub db_keyspace: String,
     pub log_level: String,
     pub output: String,
     pub modified_source_code_url: String,
     pub banner: String,
+    pub hmac_key: String,
+    pub domain: String,
+    pub use_pubsub_feeds: bool,
+    pub popular_enabled: bool,
+    pub captcha_enabled: bool,
+    pub login_enabled: bool,
+    pub registration_enabled: bool,
+    pub statistics_enabled: bool,
+    pub admins: Vec<String>,
+    pub external_port: String,
+    pub default_user_preferences: Preferences,
+    pub dmca_content: bool,
+    pub check_tables: String,
+    pub hsts: bool,
+    pub disable_proxy: bool,
+    pub port: String,
+    pub host_binding: String,
+}
+
+impl Config {
+    /// Loads the configuration and parses it into a Config object, panics if the configuration file is invalid or missing.
+    pub fn new() -> Config {
+        let loaded_config = fs::read_to_string("config.yaml").unwrap();
+        serde_yaml::from_str(&serde_yaml::to_string(&loaded_config).unwrap()).unwrap()
+    }
 }
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct Preferences {
-    pub annotations: bool,
     pub autoplay: bool,
     pub captions: Vec<String>,
     pub comments: Vec<String>,
-    pub continue_what_needs_work: bool,
+    pub load_next: bool,
     pub continue_autoplay: bool,
     pub dark_mode: String,
     pub latest_only: bool,
