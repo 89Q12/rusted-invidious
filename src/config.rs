3a0faea8730cefd40c7fe8798b467a61ce7e96f6
11tuvork28 <zofi@xamh.de>
2022-06-28 12:03:21 +0200

    added preferences and removed envconfig

--- src/config.rs
+++ src/config.rs
@@ -1,19 +1,56 @@
-use envconfig::Envconfig;
-use youtubei_rs::types::client::ClientConfig;
-#[derive(Envconfig)]
+use crate::database::db_manger::DbManager;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use youtubei_rs::types::{client::ClientConfig, query_results::BrowseResult};
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct Config {
-    #[envconfig(from = "DB_HOST")]
     pub db_host: String,
-    #[envconfig(from = "DB_PORT", default = "5432")]
     pub db_port: u16,
-    #[envconfig(from = "LOG_LEVEL", default = "debug")]
     pub log_level: String,
-    #[envconfig(from = "OUTPUT_FILE", default = "stdout")]
-    pub output: String
+    pub output: String,
+    pub modified_source_code_url: String,
+    pub banner: String,
+}
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Preferences {
+    pub annotations: bool,
+    pub autoplay: bool,
+    pub captions: Vec<String>,
+    pub comments: Vec<String>,
+    pub continue_what_needs_work: bool,
+    pub continue_autoplay: bool,
+    pub dark_mode: String,
+    pub latest_only: bool,
+    pub listen: bool,
+    pub local: bool,
+    pub locale: String,
+    pub watch_history: bool,
+    pub max_results: i32,
+    pub notifications_only: bool,
+    pub player_style: String,
+    pub quality: String,
+    pub quality_dash: String,
+    pub default_home: String,
+    pub feed_menu: Vec<String>,
+    pub automatic_instance_redirect: bool,
+    pub region: String,
+    pub related_videos: bool,
+    pub sort: String,
+    pub speed: f32,
+    pub thin_mode: bool,
+    pub unseen_only: bool,
+    pub video_loop: bool,
+    pub extend_desc: bool,
+    pub volume: i32,
+    pub vr_mode: bool,
+    pub show_nick: bool,
+    pub save_player_pos: bool,
 }
 
 /// The state that is shared with all handlers
-pub struct  State{
+pub struct State {
     pub yt_client_config: ClientConfig,
-    
+    pub db_manager: DbManager,
+    pub channel_map: HashMap<String, BrowseResult>,
 }
