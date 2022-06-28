8ef001b45d0aa45e37603dbd3370737dd4d4fab8
11tuvork28 <zofi@xamh.de>
2022-06-28 14:19:56 +0200

    added needed fields and askama localization macro

--- src/handlers/templates/base.rs
+++ src/handlers/templates/base.rs
@@ -1,12 +1,18 @@
-use askama::Template;
-use crate::{database::models::user::User, config::{Config, Preferences}};
+use askama::{Template, Locale};
+use crate::{config::{Config, Preferences}, structs::user::User};
 
+askama::localization!(LOCALES);
 
 #[derive(Template)] // this will generate the code...
 #[template(path = "base.html")]
-pub struct Base{
+pub struct Base<'a>{
     title: String,
     config: Config,
-    user: User,
+    user: Option<User>,
     preferences: Preferences,
+    headers: Vec<String>,
+    search_bar: Option<String>,
+    current_page: String,
+    #[locale]
+    loc: Locale<'a>,
 }
