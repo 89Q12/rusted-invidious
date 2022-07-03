711a71b5652253864a9c3f8afe1d108549f4f65b
11tuvork28 <zofi@xamh.de>
2022-07-03 19:40:17 +0200

    added     todo!()

--- src/handlers/video.rs
+++ src/handlers/video.rs
@@ -1,21 +1,12 @@
 use std::{sync::Arc, collections::HashMap, fmt::Debug};
-use axum::{response::{Response, Redirect, IntoResponse}, Extension,extract::{path::Path, Query}, http::StatusCode};
+use axum::{response::{Response, Redirect, IntoResponse, Html}, Extension,extract::{path::Path, Query}, http::StatusCode};
 use tokio::sync::Mutex;
 use youtubei_rs::query::{player, next_video_id, resolve};
 use crate::config::State;
 
 /// Handler for the /watch?v=id path and renders the watch page
-pub async fn watch_v(Extension(state): Extension<Arc<Mutex<State>>>,Query(params): Query<HashMap<String, String>>) -> Result<String, String> {
-    let video_id = params.get("v").unwrap();
-    let lock = state.lock().await;
-    let (player, next) = tokio::join!(player(video_id.to_owned(), "".to_string(), &lock.yt_client_config), next_video_id(video_id.to_string(), "".to_string(), &lock.yt_client_config));
-    match player{
-        Ok(pl) => Ok(pl.video_details.title),
-        Err(e) => match e{
-            youtubei_rs::types::error::Errors::RequestError(err) =>Err(err.message) ,
-            youtubei_rs::types::error::Errors::ParseError(err) => Err(err.message),
-        },
-    }
+pub async fn watch_v(Extension(state): Extension<Arc<Mutex<State>>>,Query(params): Query<HashMap<String, String>>) -> String {
+    todo!()
 }
 
 /// Handler for the paths /w/:id, /v/:id,/e/:id, /shorts/:id and /watch/:id it redirects to the /watch/v=id path
@@ -29,7 +20,7 @@ pub async fn watch_ajax(Extension(state): Extension<Arc<Mutex<State>>>,body: Str
     todo!()
 }
 
-/// Handler for the paths /clip/:id it extracts the videoId from the clip id and redirects to the /watch/v=id path
+/// Handler for the paths /clip/:id it extracts the videoId from the clip id and redirects to the /watch?v=id path
 pub async fn clip(Extension(state): Extension<Arc<Mutex<State>>>,Path(id): Path<String>) -> Redirect{
     let lock = state.lock().await;
     let video_id = resolve("https://www.youtube.com/clip/".to_owned()+ &id, &lock.yt_client_config).await;
