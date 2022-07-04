6cd870d64bf29e77d781c7b03d897d70f308a4ef
11tuvork28 <zofi@xamh.de>
2022-07-04 18:36:18 +0200

    Added templateContext that holds information about the current context such as current path, domain etc. This resolves #6

--- src/handlers/templates/base.rs
+++ src/handlers/templates/base.rs
@@ -1,5 +1,5 @@
 use askama::{Template, Locale};
-use crate::{config::{Config, Preferences}, structs::user::User};
+use crate::{config::{Config, Preferences}, structs::{user::User, template_context::TemplateContext}};
 
 #[derive(Template)] // this will generate the code...
 #[template(path = "base.html")]
@@ -12,4 +12,5 @@ pub struct Base<'a>{
     pub current_page: String,
     #[locale]
     pub loc: Locale<'a>,
+    pub params: TemplateContext
 }
--- src/structs/mod.rs
+++ src/structs/mod.rs
@@ -1,3 +1,4 @@
 pub mod user;
 pub mod Video;
 pub mod Playlist;
+pub mod template_context;
