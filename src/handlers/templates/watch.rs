b93b98a71072430a17536bca59757e7670a56e51
11tuvork28 <zofi@xamh.de>
2022-07-04 19:31:52 +0200

    implemeted TemplateContext in base.html

--- src/handlers/templates/watch.rs
+++ src/handlers/templates/watch.rs
@@ -1,18 +1,19 @@
-use askama::{Template, Locale};
+use askama::{Locale, Template};
 use youtubei_rs::types::misc::StreamingData;
 
-use crate::structs::{Video::Video, Playlist::Playlist};
+use crate::structs::{Playlist::Playlist, Video::Video};
 
 use super::base::Base;
 
 #[derive(Template)] // this will generate the code...
 #[template(path = "watch.html")]
-pub struct Watch<'a>{
+pub struct Watch<'a> {
     title: String,
     #[locale]
     loc: Locale<'a>,
     _parent: Base<'a>,
     video: Video,
     streaming_data: StreamingData,
-    playlist: Option<Playlist>
+    playlist: Option<Playlist>,
 }
+
