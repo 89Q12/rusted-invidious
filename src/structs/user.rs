06b07f45616e35a15d6d63116b514b7a662ac3f6
11tuvork28 <zofi@xamh.de>
2022-07-04 19:32:57 +0200

    Added correct return type to get_playlists

--- src/structs/user.rs
+++ src/structs/user.rs
@@ -1,10 +1,12 @@
+use super::Playlist;
+
 pub struct User{
     pub csrf_token: String,
     pub name: String,
     pub notification_count: i32,
 }
 impl User {
-    pub fn get_playlists(&self) -> Vec<String> {
+    pub fn get_playlists(&self) -> Vec<Playlist::Playlist> {
         todo!()
     }
 }
