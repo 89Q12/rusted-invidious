ead42fd9cee52a627f575c94fd5caaeadd28004b
11tuvork28 <zofi@xamh.de>
2022-07-05 11:24:48 +0200

    removed unused field from watch struct

--- src/handlers/templates/watch.rs
+++ src/handlers/templates/watch.rs
@@ -8,7 +8,6 @@ use super::base::Base;
 #[derive(Template)] // this will generate the code...
 #[template(path = "watch.html")]
 pub struct Watch<'a> {
-    title: String,
     #[locale]
     loc: Locale<'a>,
     _parent: Base<'a>,
