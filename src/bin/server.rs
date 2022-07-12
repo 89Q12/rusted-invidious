221ddbe760303386e218e40ba51c5b747855593c
11tuvork28 <zofi@xamh.de>
2022-07-12 16:30:25 +0200

    implements opentelemetry

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,20 +1,27 @@
 use axum::Extension;
 use rusted_invidious_types::database::db_manager::DbManager;
 use rusted_invidious::{routes::get_router, config::{Config, State, Preferences}, logging::filters::construct_logging_filter};
+use youtubei_rs::utils::default_client_config;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 use tower_http::trace::TraceLayer;
+use opentelemetry::global;
 use std::net::SocketAddr;
 use tokio::sync::Mutex;
 use std::sync::Arc;
-use youtubei_rs::utils::default_client_config;
 /// Main entry point for the web server:
 /// it setups logging, loads the configuration, inits the database api and starts the server
 #[tokio::main]
 async fn main() {
     // TODO: implement configuration via env file
     let config = Config::new();
+    global::set_text_map_propagator(opentelemetry_jaeger::Propagator::new());
+    let tracer = opentelemetry_jaeger::new_pipeline().install_simple().unwrap();
+
+    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+
     tracing_subscriber::registry()
     .with(construct_logging_filter())
+    .with(telemetry)
     .with(tracing_subscriber::fmt::layer())
     .init();
 
@@ -42,5 +49,5 @@ async fn main() {
         .serve(app.into_make_service())
         .await
         .unwrap();
-
+     global::shutdown_tracer_provider();
 }
