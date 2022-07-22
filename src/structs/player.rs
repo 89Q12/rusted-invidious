a4e07d6a1cd049d55883917668da1e7b6525f0c2
11tuvork28 <zofi@xamh.de>
2022-07-22 19:41:04 +0200

    Update youtubei and apdated breaking changes

--- src/structs/player.rs
+++ src/structs/player.rs
@@ -1,7 +1,7 @@
-use youtubei_rs::types::misc::{Format, CaptionTrack};
+use youtubei_rs::types::video::{Format, CaptionTrack};
 
 pub struct Player<'a>{
-    pub formats: &'a Vec<Format>,
+    pub formats: &'a Vec<&'a Format>,
     pub audio_streams: Vec<&'a Format>,
     pub captions: &'a Vec<CaptionTrack>,
     pub preferred_captions: Vec<&'a CaptionTrack>,
