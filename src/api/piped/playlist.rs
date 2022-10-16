b6942fbce6f1890582ea136a29f6503f1209fb7e
11Tuvork28 <zofi@xamh.de>
2022-10-16 16:17:23 +0200

    All the pub fields hahah

--- src/api/piped/playlist.rs
+++ src/api/piped/playlist.rs
@@ -5,15 +5,15 @@ use super::misc::RelatedStream;
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Playlist {
-    banner_url: String, // The banner of the playlist
-    name: String, // The name of the playlist
-    nextpage: String, // A JSON encoded page, which is used for the nextpage endpoint.
-    related_streams: Vec<RelatedStream>, // A list of videos from the playlist
-    thumbnail_url: String, // The thumbnail of the playlist
-    uploader: String, // The name of the creator of the playlist
-    uploader_svatar: String, // The avatar of the creator of the playlist
-    uploader_url: String, // The URL of the creator of the playlist
-    videos: i32 // The number of videos in the playlist
+    pub banner_url: String, // The banner of the playlist
+    pub name: String, // The name of the playlist
+    pub nextpage: String, // A JSON encoded page, which is used for the nextpage endpoint.
+    pub related_streams: Vec<RelatedStream>, // A list of videos from the playlist
+    pub thumbnail_url: String, // The thumbnail of the playlist
+    pub uploader: String, // The name of the creator of the playlist
+    pub uploader_svatar: String, // The avatar of the creator of the playlist
+    pub uploader_url: String, // The URL of the creator of the playlist
+    pub videos: i32 // The number of videos in the playlist
 }
 
 #[derive(Deserialize)]
--- src/api/piped/video.rs
+++ src/api/piped/video.rs
@@ -6,68 +6,69 @@ use super::misc::RelatedStream;
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Video{
-    video_streams: Vec<VideoStream>,  // The video streams of the video
-    audio_streams: Vec<AudioStream>,  // The audio streams of the video
-    subtitles: Vec<Subtitle>, // List of subtitles
-    related_streams: Vec<RelatedStream>, // A list of related streams
-    chapters: Option<Vec<Chapter>>, // Possible list of chapters
-    dash: Option<String>,  // The dash manifest URL, to be used if not null (for OTF streams)
-    hls: Option<String>, // The hls manifest URL, to be used for Livestreams
-    description: String,  // The description of the video
-    duration: i32,  // The duration of the video in seconds
-    likes: i32, // The number of likes the video has
-    lifestream: bool,  // Whether or not the video is a livestream
-    proxy_url: String, // The proxy url to be used for rewrites
-    thumbnail_url: String, // The thumbnail of the video
-    title: String,  // The title of the video
-    uploaded_date: String, // The date the video was uploaded
-    uploader: String,  // The name of the channel of the video
-    uploader_url: String, // The URL of the channel of the video
-    uploader_verified: bool,  // Whether or not the channel of the video is verified
-    uploader_subscriber_count: i32, // Subscribercount of the author(Channel)
-    views: i32, // The number of views the video has
+    pub video_streams: Vec<VideoStream>,  // The video streams of the video
+    pub audio_streams: Vec<AudioStream>,  // The audio streams of the video
+    pub subtitles: Vec<Subtitle>, // List of subtitles
+    pub related_streams: Vec<RelatedStream>, // A list of related streams
+    pub chapters: Option<Vec<Chapter>>, // Possible list of chapters
+    pub dash: Option<String>,  // The dash manifest URL, to be used if not null (for OTF streams)
+    pub hls: Option<String>, // The hls manifest URL, to be used for Livestreams
+    pub description: String,  // The description of the video
+    pub duration: i32,  // The duration of the video in seconds
+    pub likes: i32, // The number of likes the video has
+    pub livestream: bool,  // Whether or not the video is a livestream
+    pub proxy_url: String, // The proxy url to be used for rewrites
+    pub thumbnail_url: String, // The thumbnail of the video
+    pub title: String,  // The title of the video
+    pub uploaded_date: String, // The date the video was uploaded
+    pub uploader: String,  // The name of the channel of the video
+    pub uploader_url: String, // The URL of the channel of the video
+    pub uploader_verified: bool,  // Whether or not the channel of the video is verified
+    pub uploader_subscriber_count: i32, // Subscribercount of the author(Channel)
+    pub uploader_avatar: String, // The avatar url to the channel
+    pub views: i32, // The number of views the video has
 }
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct AudioStream{
-    bitrate: i32, // The bitrate of the audio stream in bytes
-    codec: String, // The codec of the audio stream
-    format: String, // The format of the audio stream
-    index_end: i32, // Useful for creating dash streams
-    index_start: i32,  // Useful for creating dash streams
-    init_start: i32,  // Useful for creating dash streams
-    init_end: i32, // Useful for creating dash streams
-    mime_type: String, // The mime type of the audio stream
-    quality: String, // The quality of the audio stream
-    url: String, // The stream's URL
-    video_only: bool, // Whether or not the stream is video only
+    pub bitrate: i32, // The bitrate of the audio stream in bytes
+    pub codec: String, // The codec of the audio stream
+    pub format: String, // The format of the audio stream
+    pub index_end: i32, // Useful for creating dash streams
+    pub index_start: i32,  // Useful for creating dash streams
+    pub init_start: i32,  // Useful for creating dash streams
+    pub init_end: i32, // Useful for creating dash streams
+    pub mime_type: String, // The mime type of the audio stream
+    pub quality: String, // The quality of the audio stream
+    pub url: String, // The stream's URL
+    pub video_only: bool, // Whether or not the stream is video only
 }
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct VideoStream{
-    bitrate: i32, // The bitrate of the video stream in bytes
-    codec: String, // The codec of the video stream
-    format: String, // The format of the video stream
-    fps: i32, // The frames per second of the video stream
-    height: i32, // The height of the video stream
-    index_end: i32, // Useful for creating dash streams
-    index_start: i32, // Useful for creating dash streams
-    init_start: i32, // Useful for creating dash streams
-    init_end: i32, // Useful for creating dash streams
-    mime_type: String, // The mime type of the video stream
-    quality: String, // The quality of the video stream
-    url: String, // The stream's URL
-    video_only: bool, // Whether or not the stream is video only
-    width: i32 // The width of the video stream
+    pub bitrate: i32, // The bitrate of the video stream in bytes
+    pub codec: String, // The codec of the video stream
+    pub format: String, // The format of the video stream
+    pub fps: i32, // The frames per second of the video stream
+    pub height: i32, // The height of the video stream
+    pub index_end: i32, // Useful for creating dash streams
+    pub index_start: i32, // Useful for creating dash streams
+    pub init_start: i32, // Useful for creating dash streams
+    pub init_end: i32, // Useful for creating dash streams
+    pub mime_type: String, // The mime type of the video stream
+    pub quality: String, // The quality of the video stream
+    pub url: String, // The stream's URL
+    pub video_only: bool, // Whether or not the stream is video only
+    pub width: i32 // The width of the video stream
 }
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Subtitle{
-    auto_generated: bool, // Whether or not the subtitle was auto-generated
-    code: String, // The language code of the subtitle
-    mime_type: String, // The mime type of the subtitle
-    name: String, // The name of the subtitle
-    url: String, // The URL of the subtitle
+    pub auto_generated: bool, // Whether or not the subtitle was auto-generated
+    pub code: String, // The language code of the subtitle
+    pub mime_type: String, // The mime type of the subtitle
+    pub name: String, // The name of the subtitle
+    pub url: String, // The URL of the subtitle
 }
 #[derive(Deserialize)]
 pub struct Chapter{
@@ -79,5 +80,16 @@ pub struct Chapter{
 impl From<Value> for Video{
     fn from(value: Value) -> Self {
         serde_json::from_value(value).unwrap()
+    }
+}
+impl Video{
+    pub fn get_next_video(&self) -> String{
+        match self.related_streams.get(0){
+            Some(stream) => stream.get_id(),
+            None => String::from(""),
+        }
+    }
+    pub fn get_channel_id(&self) -> String{
+        self.uploader_url.split("/").map(|x| x.to_string()).collect::<Vec<String>>()[2]
     }
 }
