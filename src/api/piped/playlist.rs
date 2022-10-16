dc0f56565d02f9c150cd53bd257f426f2415d29b
11Tuvork28 <zofi@xamh.de>
2022-10-16 17:01:22 +0200

    Made fields pub

--- src/api/piped/playlist.rs
+++ src/api/piped/playlist.rs
@@ -5,13 +5,13 @@ use super::misc::RelatedStream;
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Playlist {
-    pub banner_url: String, // The banner of the playlist
+    pub banner_url: Option<String>, // The banner of the playlist
     pub name: String, // The name of the playlist
     pub nextpage: String, // A JSON encoded page, which is used for the nextpage endpoint.
     pub related_streams: Vec<RelatedStream>, // A list of videos from the playlist
     pub thumbnail_url: String, // The thumbnail of the playlist
     pub uploader: String, // The name of the creator of the playlist
-    pub uploader_svatar: String, // The avatar of the creator of the playlist
+    pub uploader_avatar: String, // The avatar of the creator of the playlist
     pub uploader_url: String, // The URL of the creator of the playlist
     pub videos: i32 // The number of videos in the playlist
 }
@@ -19,11 +19,11 @@ pub struct Playlist {
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct SearchPlaylist {
-    name: String, // The name of the playlist
-    url: String, // url to visit
-    thumbnail: String, // The thumbnail of the playlist
-    uploader_name: String, // The name of the creator of the playlist
-    videos: i32 // The number of videos in the playlist
+    pub name: String, // The name of the playlist
+    pub url: String, // url to visit
+    pub thumbnail: String, // The thumbnail of the playlist
+    pub uploader_name: String, // The name of the creator of the playlist
+    pub videos: i32 // The number of videos in the playlist
 }
 
 
