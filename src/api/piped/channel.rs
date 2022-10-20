bddf4c54ac3ca0dd78ac5f9e7b94a4417360c4a7
11Tuvork28 <zofi@xamh.de>
2022-10-20 21:43:29 +0200

    Implemented ChannelTrait for piped::Channel

--- src/api/piped/channel.rs
+++ src/api/piped/channel.rs
@@ -1,7 +1,10 @@
 use serde::Deserialize;
 use serde_json::Value;
 
-use super::{misc::RelatedStream, utils::parse_related};
+use crate::api::ChannelTrait;
+use crate::api::error::{ApiError, Errors};
+use crate::api::PartialVideoTrait;
+use super::misc::RelatedStream;
 //Represent the video tab ONLY
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -17,23 +20,55 @@ pub struct Channel{
     pub verified: bool // Whether or not the channel is verified
 }
 
-impl From<Value> for Channel{
-    fn from(value: Value) -> Self {
-        serde_json::from_value(value).unwrap()
-    }
-}
-impl Channel {
-    pub(super) fn manual_parse(value: Value) -> Channel {
-        Channel{
-            avatar_url: value["avatar_url"].to_string(),
-            banner_url: value["banner_url"].to_string(),
-            description: value["description"].to_string(),
-            id: value["avatar_url"].to_string(),
-            name: value["avatar_url"].to_string(),
-            nextpage: value["avatar_url"].to_string(),
-            related_streams: parse_related(value),
-            subscriber_count: value["subscriber_count"].to_string().parse().unwrap(),
-            verified: value["verified"].as_bool().unwrap(),
+impl TryFrom<Value> for Channel{
+    type Error = ApiError;
+
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        match serde_json::from_value(value){
+            Ok(val) => Ok(val),
+            Err(err) => Err(ApiError::new(Errors::RequestError, err.to_string())),
         }
     }
+}
+impl ChannelTrait for Channel {
+    fn get_avatar_url(&self) -> String {
+        self.avatar_url.clone()
+    }
+
+    fn get_banner_url(&self) -> String {
+        self.banner_url.clone()
+    }
+
+    fn get_description(&self) -> String {
+        self.description.clone()
+    }
+
+    fn get_id(&self) -> String {
+        self.id.clone()
+    }
+
+    fn get_name(&self) -> String {
+        self.name.clone()
+    }
+
+    fn get_nextpage(&self) -> String {
+        self.nextpage.clone()
+    }
+
+    fn get_related_streams(&self) -> Vec<Box<dyn PartialVideoTrait>> {
+        let mut ret_vec = Vec::new();
+        for stream in self.related_streams.to_owned(){
+            ret_vec.push(Box::new(stream) as Box<dyn PartialVideoTrait>);
+        };
+        ret_vec
+    }
+
+    fn get_subscriber_count(&self) -> i32 {
+        self.subscriber_count.clone()
+    }
+
+    fn is_verified(&self) -> bool {
+        self.verified.clone()
+    }
+
 }
