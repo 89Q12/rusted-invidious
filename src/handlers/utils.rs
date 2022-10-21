4990f225729425a6317bc2628ec5fdc8fd7e4c0a
11Tuvork28 <zofi@xamh.de>
2022-10-21 15:04:13 +0200

    added utility functions for Error/NotImplemented templates

--- src/handlers/utils.rs
+++ src/handlers/utils.rs
@@ -1,6 +1,8 @@
 use axum::{http::StatusCode,response::Response, body};
-use crate::config::Config;
-
+use askama::{langid, Template};
+use crate::api::error::ApiError;
+use crate::handlers::templates::TemplateContext;
+askama::localization!(LOCALES);
 pub fn render(html_str: Result<String, askama::Error>) -> Response { 
     return Response::builder()
     .status(StatusCode::OK)
@@ -9,4 +11,19 @@ pub fn render(html_str: Result<String, askama::Error>) -> Response {
         Err(err) => body::Full::from(err.to_string()),
     }))
     .unwrap();
+}
+
+pub fn render_error(error: ApiError,context: TemplateContext)-> Response{
+    render(super::templates::error::Error{
+        loc: askama::Locale::new(langid!("en-US"), &LOCALES),
+        context,
+        error,
+    }.render())
+}
+
+pub fn render_not_implemented(context: TemplateContext) -> Response {
+    render(super::templates::not_implemented::NotImplemented{
+        loc: askama::Locale::new(langid!("en-US"), &LOCALES),
+        context,
+    }.render())
 }
