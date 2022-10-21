04115cc602795c8fb3326487fbc7971ac202cced
11Tuvork28 <zofi@xamh.de>
2022-10-21 15:04:50 +0200

    Implemented all supported Channel endpoints

--- src/handlers/channel.rs
+++ src/handlers/channel.rs
@@ -1,67 +1,81 @@
-use crate::handlers::templates::TemplateContext;
-use crate::{config::State, api::piped::Channel};
 use axum::{
     extract::path::Path,
     response::{Redirect, Response},
-    Extension, http::{StatusCode, Request}, body::Body,
+    Extension, http::Request, body::Body,
 };
+use reqwest::ClientBuilder;
 use std::sync::Arc;
-use tokio::sync::Mutex;
+use tokio::sync::RwLock;
 use askama::{langid, Template};
-use super::utils::{string_to_body, proxyfi_url, render};
+use super::utils::{render, render_error, render_not_implemented};
+use crate::config::State;
+use crate::{handlers::templates::TemplateContext, api::piped::PipedApi};
 askama::localization!(LOCALES);
+
 /// Handler for the path /channel/:id, /c/:id, /user/:id
 pub async fn index(
-    Extension(state): Extension<Arc<Mutex<State>>>,
+    Extension(state): Extension<Arc<RwLock<State>>>,
     Path(id): Path<String>,
     request: Request<Body>
 ) -> Response {
-    let context = TemplateContext::new(&request, None, &state.lock().await.config);
-    let template = super::templates::channel::ChannelTemplate{
-        loc: askama::Locale::new(langid!("en-US"), &LOCALES),
-        channel: todo!(),
-        context,
-    };
-    render(template.render())
+  let client = ClientBuilder::new().gzip(true).build().unwrap();
+  let piped = PipedApi::new(client).api_host("http://localhost:8080".to_owned()).build();
+  let config = &state.read().await.config;
+  let context = TemplateContext::new(&request, None, config);
+  let channel = match piped.get_channel(id).await{
+        Ok(chan) => chan,
+        Err(err) => return render_error(err, context) ,
+  };
+  let template = super::templates::channel::ChannelTemplate{
+      loc: askama::Locale::new(langid!("en-US"), &LOCALES),
+      channel,
+      context,
+  };
+  render(template.render())
 }
 
 /// Handler for the path /channel/:id/videos, /c/:id/videos, /user/:id/videos
 pub async fn videos(
-    Extension(state): Extension<Arc<Mutex<State>>>,
     Path(id): Path<String>,
-) -> Response {
-    todo!()
+) -> Redirect {
+    Redirect::temporary(&format!("/channel/{}/",id))
 }
 
 /// Handler for the path /channel/:id/playlists, /c/:id/playlists, /user/:id/playlists
 pub async fn playlists(
-    Extension(state): Extension<Arc<Mutex<State>>>,
-    Path(id): Path<String>,
+    Extension(state): Extension<Arc<RwLock<State>>>,
     request: Request<Body>
 ) -> Response {
-    todo!()
+    let config = &state.read().await.config;
+    let context = TemplateContext::new(&request, None, config);
+    render_not_implemented(context)
 }
 
 /// Handler for the path /channel/:id/community, /c/:id/community, /user/:id/community
 pub async fn community(
-    Extension(state): Extension<Arc<Mutex<State>>>,
-    Path(id): Path<String>,
+    Extension(state): Extension<Arc<RwLock<State>>>,
     request: Request<Body>
 ) -> Response {
-    todo!()
+    let config = &state.read().await.config;
+    let context = TemplateContext::new(&request, None, config);
+    render_not_implemented(context)
 }
 /// Handler for the path /channel/:id/live, /c/:id/, /user/:id/live
 pub async fn live(
-    Extension(_state): Extension<Arc<Mutex<State>>>,
-    Path(_id): Path<String>,
-) -> Redirect {
-    todo!()
+    Extension(state): Extension<Arc<RwLock<State>>>,
+    request: Request<Body>
+) -> Response {
+    let config = &state.read().await.config;
+    let context = TemplateContext::new(&request, None, config);
+    render_not_implemented(context)
 }
 
 /// Handler for the path /attribution_link?a=something&u=/channel/:id/ it resolves the url and redirects to /channel/:id/
 pub async fn attribution_link(
-    Extension(_state): Extension<Arc<Mutex<State>>>,
-    Path(_id): Path<String>,
-) -> Redirect {
-    todo!()
+    Extension(state): Extension<Arc<RwLock<State>>>,
+    request: Request<Body>
+) -> Response {
+    let config = &state.read().await.config;
+    let context = TemplateContext::new(&request, None, config);
+    render_not_implemented(context)
 }
