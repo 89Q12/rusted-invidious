72c9922b50e0c29e75ba2eabd3f9a428883c64fa
11Tuvork28 <zofi@xamh.de>
2022-10-21 16:05:36 +0200

    Renamed not implemented template to correct Name NotImplemented

--- src/handlers/templates/not_implemented.rs
+++ src/handlers/templates/not_implemented.rs
@@ -2,7 +2,7 @@ use askama::{Locale, Template};
 use super::TemplateContext;
 #[derive(Template)]
 #[template(path = "not_implemented.html")]
-pub struct Error<'a> {
+pub struct NotImplemented<'a> {
     #[locale]
     pub loc: Locale<'a>,
     pub context: TemplateContext<'a>,
