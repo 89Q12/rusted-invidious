0b6bcdb1d94c335680449bfcc33d7a5c4959cdb7
11Tuvork28 <zofi@xamh.de>
2022-10-23 10:03:02 +0200

    Implemented TryFrom<Value> for piped::Search

--- src/api/piped/search.rs
+++ src/api/piped/search.rs
@@ -1,7 +1,11 @@
 use serde::Deserialize;
+use serde_json::Value;
+use serde_json;
+use crate::api::error::{ApiError, Errors};
 
 use super::{SearchPlaylist, RelatedStream};
 
+#[derive(Deserialize)]
 pub struct Search {
     nextpage: String, // A JSON encoded page, which is used for the nextpage endpoint.
     suggestion: Option<String>, // Something something idk
@@ -12,4 +16,15 @@ pub struct Search {
 pub enum SearchItem {
     SearchPlaylist(SearchPlaylist),
     RelatedStream(RelatedStream)
+}
+
+impl TryFrom<Value> for Search{
+    type Error = ApiError;
+
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        match serde_json::from_value(value){
+            Ok(val) => Ok(val),
+            Err(err) => Err(ApiError::new(Errors::RequestError, err.to_string())),
+        }
+    }
 }
