6d9fcf59081deaf8824eaa9a51178d2959459f7c
11Tuvork28 <zofi@xamh.de>
2022-10-23 10:04:31 +0200

    Set cors for development, so that its not getting in the way

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,11 +1,13 @@
-use axum::Extension;
+use axum::{Extension, http::HeaderValue};
+use reqwest::Method;
 use rusted_invidious_types::database::db_manager::DbManager;
 use rusted_invidious::{routes::get_router, config::{Config, State, Preferences}, logging::filters::construct_logging_filter};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
-use tower_http::trace::TraceLayer;
+use tower_http::{trace::TraceLayer, cors::Any};
 use std::net::SocketAddr;
 use tokio::sync::RwLock;
 use std::sync::Arc;
+use tower_http::cors::CorsLayer;
 /// Main entry point for the web server:
 /// it setups logging, loads the configuration, inits the database api and starts the server
 #[tokio::main]
@@ -31,7 +33,8 @@ async fn main() {
     }));
 
 
-    let app = get_router().layer(TraceLayer::new_for_http()).layer(Extension(shared_state));
+    let app = get_router().layer(CorsLayer::new()
+    .allow_origin("http://localhost".parse::<HeaderValue>().unwrap()).allow_origin(Any).allow_methods(Any).allow_headers(Any)).layer(TraceLayer::new_for_http()).layer(Extension(shared_state));
     
     let addr = SocketAddr::from(([172, 17, 0, 1], 3000));
 
